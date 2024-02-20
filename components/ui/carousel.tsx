06bd9e5329cecd8962ea02dbdbfab1cfe656ec24
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-02-20 16:18:50 +0530

    Update carousel.tsx

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -11,6 +11,22 @@ import {
   type EmblaPluginType as CarouselPlugin,
 } from 'embla-carousel';
 
+type CarouselProps = {
+  opts?: CarouselOptions;
+  plugins?: CarouselPlugin[];
+  orientation?: "horizontal" | "vertical";
+  setApi?: (api: CarouselApi) => void;
+};
+
+type CarouselContextProps = {
+  carouselRef: ReturnType<typeof useEmblaCarousel>[0];
+  api: ReturnType<typeof useEmblaCarousel>[1];
+  scrollPrev: () => void;
+  scrollNext: () => void;
+  canScrollPrev: boolean;
+  canScrollNext: boolean;
+} & CarouselProps;
+
 
 const CarouselContext = React.createContext<CarouselContextProps | null>(null)
 
